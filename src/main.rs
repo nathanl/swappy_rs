3bd5f9a8d0024770280be681b46932ac08f00857
Nathan Long <him@nathanmlong.com>
2020-04-08 15:52:51 -0400

    Allow specifying phrase, word list, and limit as CLI args

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,45 @@ mod candidate_anagram;
 mod priority;
 mod solver;
 mod word_list;
+use std::env;
 
 fn main() {
-    // let word_list = word_list::lines_from_file("/usr/share/dict/words");
-    let word_list = word_list::lines_from_file(
-        "/Users/nathanlong/code/mine/anagram_wordlists/smallish_list.txt",
-    );
-    let results = solver::anagrams_for("rust language".to_string(), &word_list, 1000);
-    for r in results {
-        println!("result is {}", r);
+    let args: Vec<String> = env::args().collect();
+    let phrase: &str;
+    match args.get(1) {
+        Some(input) => {
+            phrase = &input;
+        }
+        None => {
+            print_usage();
+            std::process::exit(1);
+        }
     }
+
+    let word_list_file = match env::var("WORDS") {
+        Ok(filename) => filename.to_string(),
+        Err(_) => "test_support/smallish_list.txt".to_string()
+    };
+
+    let limit: usize = match env::var("LIMIT") {
+        Ok(val) => {
+            val.parse::<usize>().unwrap()
+        },
+        Err(_) => 1_000
+    };
+
+    let word_list = word_list::lines_from_file(word_list_file);
+    let results = solver::anagrams_for(phrase.to_string(), &word_list, limit);
+    for r in &results {
+        println!("{}", r);
+    }
+    eprintln!("{} total results", results.len());
+}
+
+fn print_usage() {
+    println!("
+    USAGE:
+       cargo run 'my phrase'
+       LIMIT=3 WORDS=/some/file.txt 'my phrase'
+    ");
 }
