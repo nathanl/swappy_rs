8f015e5c96b8c6bd804c4e2e5321cbe87a09b234
Nathan Long <him@nathanmlong.com>
2020-05-29 11:40:10 -0400

    WIP

--- src/solver.rs
+++ src/solver.rs
@@ -2,9 +2,7 @@ use crate::alphagram::Alphagram;
 use crate::candidate_anagram::CandidateAnagram;
 use crate::priority::Priority;
 use crate::word_list;
-use priority_queue::PriorityQueue;
 use std::time::Instant;
-// use keyed_priority_queue::KeyedPriorityQueue as PriorityQueue;
 
 
 // I failed, Nathan.  I don't know rust well enough and the lag over this connection is pretty bad.
@@ -36,7 +34,7 @@ Things I don't know how to do yet, if I could even get the above code typed in:
  *
  * Our data structures are:
  *   - Alphagram
- *   - CandidateAnagram has the remainining characters and the words found so far (as a list of
+ *   - CandidateAnagram has the remaining characters and the words found so far (as a list of
  *   numbers)
  *
  * Game plan:
@@ -57,59 +55,60 @@ pub fn anagrams_for(
     let word_list = word_list::words_with_alphagrams(word_list);
     let word_list = word_list::found_within(word_list, user_input.clone());
     eprintln!("Prepped the word list");
-    let mut pq = PriorityQueue::new();
 
     let mut now = Instant::now();
 
     let c = CandidateAnagram::new(&user_input);
-    pq.push(c, Priority::new(vec![]));
 
-    loop {
-        attempts += 1;
-        if attempts % 100_000 == 0 {
-            let elapsed = now.elapsed().as_millis();
-            eprintln!(
-                "{} attempts, {} results, pq length {}, elapsed {:?}",
-                attempts,
-                results.len(),
-                pq.len(),
-                elapsed
-            );
-            now = Instant::now();
-        }
-        let popped = pq.pop();
-        if popped.is_none() {
-            return results;
-        }
-        let (candidate, priority) = popped.unwrap();
-        if candidate.is_complete() {
-            let result_string = priority_to_string(&priority, &word_list);
-            // eprintln!("{}", result_string);
-            results.push(result_string);
-            if results.len() >= requested_length {
-                return results;
-            }
-            continue;
-        }
-        let word_index = candidate.next_word;
-        let word_list_alphagram = &word_list[word_index].1;
-        // take out apple and remember that the next word to try is number 1 (apple)
-        let without_result = candidate.without(&word_list_alphagram, word_index);
-        match without_result {
-            Ok(new_candidate) => {
-                pq.push(new_candidate, priority.plus(word_index));
-                ()
-            }
-            Err(_) => (),
-        }
-
-        // if we haven't run out of words, also try looking for the next word
-        if candidate.next_word + 1 < word_list.len() {
-            let next_candidate = candidate.advanced_by(1);
-            pq.push(next_candidate, priority);
-            // println!("pq len {}", pq.len());
-        }
-    }
+    return results;
+    // pq.push(c, Priority::new(vec![]));
+    //
+    // loop {
+    //     attempts += 1;
+    //     if attempts % 100_000 == 0 {
+    //         let elapsed = now.elapsed().as_millis();
+    //         eprintln!(
+    //             "{} attempts, {} results, pq length {}, elapsed {:?}",
+    //             attempts,
+    //             results.len(),
+    //             pq.len(),
+    //             elapsed
+    //         );
+    //         now = Instant::now();
+    //     }
+    //     let popped = pq.pop();
+    //     if popped.is_none() {
+    //         return results;
+    //     }
+    //     let (candidate, priority) = popped.unwrap();
+    //     if candidate.is_complete() {
+    //         let result_string = priority_to_string(&priority, &word_list);
+    //         // eprintln!("{}", result_string);
+    //         results.push(result_string);
+    //         if results.len() >= requested_length {
+    //             return results;
+    //         }
+    //         continue;
+    //     }
+    //     let word_index = candidate.next_word;
+    //     let word_list_alphagram = &word_list[word_index].1;
+    //     // take out apple and remember that the next word to try is number 1 (apple)
+    //     let without_result = candidate.without(&word_list_alphagram, word_index);
+    //     match without_result {
+    //         Ok(new_candidate) => {
+    //             pq.push(new_candidate, priority.plus(word_index));
+    //             ()
+    //         }
+    //         Err(_) => (),
+    //     }
+    //
+    //     // if we haven't run out of words, also try looking for the next word
+    //     if candidate.next_word + 1 < word_list.len() {
+    //         let next_candidate = candidate.advanced_by(1);
+    //         pq.push(next_candidate, priority);
+    //         // println!("pq len {}", pq.len());
+    //     }
+    // }
 }
 
 fn priority_to_string(priority: &Priority, word_list: &Vec<(&String, Alphagram)>) -> String {
